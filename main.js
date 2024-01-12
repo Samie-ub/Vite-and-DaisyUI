e06a8a5b3489e29730189ab6c28691f757cdc1f7
Samie-ub <xxxsamie39@gmail.com>
2024-01-12 11:34:38 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+import "./style.css";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const themeToggle = document.querySelector(".swap");
+
+  themeToggle.addEventListener("change", function () {
+    document.body.classList.toggle("light-theme");
+  });
+});
